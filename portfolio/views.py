ceb889aea809183efb36d2a80aeb714bd69dcd9d
beadsampaio <beatrizdsampaio@gmail.com>
2023-05-24 09:02:01 +0100

    project config

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render
+
+
+def home_page_view(request):
+    return render(request, 'portfolio/home.html')
